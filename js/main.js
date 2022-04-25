28f8d2b177fd53e7fa1bc62aed07c727fe019702
Aidarbek Umurzakov <aidarali.prod@gmail.com>
2022-04-25 17:48:08 +0600

    changed

--- js/main.js
+++ js/main.js
@@ -13,80 +13,77 @@ let upperCase = /[A-Z]/g;
 let lowerCase = /[a-z]/g;
 let nums = /[0-9]/g;
 
-
-let dataBox = []
-let dataBoxParse = JSON.parse((localStorage.getItem('users')))
+let data = JSON.parse((localStorage.getItem('users')))
 
 
 function signUp(){
     
-    if(dataBox.some(item => item.email === emailEL.value)){
+    if(data.find(item => item.email === emailEL.value)){
         emailEL.value = '';
-        usernameEL.value = '';
-        passwordEL.value = '';
-        confirmPasswordEL.value = '';
         return alert('This email is already taken')
     }
-    else if(dataBox.some(item => item.username === usernameEL.value)){
-        emailEL.value = '';
+    else if(data.find(item => item.username === usernameEL.value)){
         usernameEL.value = '';
-        passwordEL.value = '';
-        confirmPasswordEL.value = '';
         return alert('This username is already taken')
     }
    if(usernameEL.value === "" || emailEL.value === "" || passwordEL.value === "" || confirmPasswordEL.value === ""){
     
     }
     else if(usernameEL.value === username){
-        alert('erfefcd')
+        return alert('erfefcd')
+        
     }
     else if(usernameEL.value.length < 5){
         usernameEL.style.border ="1px solid red"
     }
     else if(!passwordEL.value.match(upperCase)){
-        alert('Need 1 UpperCase length')
         passwordEL.style.border ="1px solid red"
         confirmPasswordEL.style.border ="1px solid red"
+       return alert('Need 1 UpperCase length');
     }
     else if(!passwordEL.value.match(lowerCase)){
-        alert('Need LowerCase lengths')
         passwordEL.style.border ="1px solid red"
+        return alert('Need LowerCase lengths')
         
     }
     else if(!passwordEL.value.match(nums)){
-        alert('Need numbers')
         passwordEL.style.border ="1px solid red"
+        return alert('Need numbers')
 
     }
     else if(!passwordEL.value === confirmPasswordEL.value){
-        alert('Another password in Second field')
         confirmPasswordEL.style.border ="1px solid red"
+       return alert('Another password in Second field')
     }
     else if(passwordEL.value.length <= 8){
         passwordEL.style.border ="1px solid red"
     }
     else if(usernameEL.value > 0 || emailEL.value > 0 || passwordEL.value.length >= 8 && confirmPasswordEL.value === passwordEL.value){
-        dataBox.push({
+        data.push({
         username: usernameEL.value,
         email: emailEL.value,
         password: passwordEL.value
      })
     }
     else{
+        
        alert('Error')
     }
-        console.log(dataBox);
+        console.log(data);
+
+        let dataJson = JSON.stringify(data)
+        localStorage.setItem('users',dataJson)
         
 }
 
 function logIn(){
-    if (dataBox.some(item => item.email === emailLoginEL.value && item.password === passwordLoginEl.value)){
+    if (data.some(item => item.email === emailLoginEL.value && item.password === passwordLoginEl.value)){
         window.open("/html/hello.html")
     }
-    else if(dataBox.some(item => item.password !== passwordLoginEl.value)){
+    else if(data.some(item => item.password !== passwordLoginEl.value)){
         passwordLoginEl.style.border ="1px solid red"
     }
-    else if(dataBox.some(item => item.email !== emailLoginEL.value)){
+    else if(data.some(item => item.email !== emailLoginEL.value)){
         emailLoginEL.style.border ="1px solid red"
         
     }
